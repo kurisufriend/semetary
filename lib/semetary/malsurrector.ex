69316b27c73009674e23d02d25b83e8b586d5d17
cynic <kurisufag1@gmail.com>
2024-03-10 00:52:39 -0500

    dynamic endpoint management

--- lib/semetary/malsurrector.ex
+++ lib/semetary/malsurrector.ex
@@ -1,42 +1,68 @@
 defmodule Semetary.Malsurrector do
+
+  @endpoints %{
+    "rentry" => %{
+      "urls" => ["rentry.co", "rentry.org"],
+      "handler" => &Semetary.Malsurrector.handle_rentry/1
+    },
+    "litter.catbox.moe" => %{
+      "urls" => ["litter.catbox.moe"],
+      "handler" => &Semetary.Malsurrector.handle_litter/1
+    },
+    "vocaroo.com" => %{
+      "urls" => ["vocaroo.com"],
+      "handler" => &Semetary.Malsurrector.handle_vocaroo/1
+    },
+    "pastebin.com" => %{
+      "urls" => ["pastebin.com"],
+      "handler" => &Semetary.Malsurrector.handle_pastebin/1
+    }
+  }
+
   def post_processing(post) do
     if post["com"] do
       body = post["com"] |> String.replace("<wbr>", "")
-      instances = Regex.scan(~r/(|https:\/\/|http:\/\/)(rentry.co|rentry.org|litter.catbox.moe|vocaroo.com|pastebin.com)\/[^ \n<>]*/, body)
+      url_capture = @endpoints |> Map.keys |> Enum.map(fn key -> @endpoints[key]["urls"] end)
+        |> List.flatten |> Enum.join("|")
+      instances = (Regex.compile!("(|https:\/\/|http:\/\/)(#{url_capture})\/[^ \n<>]*")
+       |> Regex.scan(body))
       if instances != [] do
         instances
         |> Enum.each(fn i ->
-          cond do
-            i |> String.contains("rentry") ->
-              _
-            i |> String.contains("litter.catbox.moe") ->
-              _
-            i |> String.contains("vocaroo.com") ->
-              _
-            i |> String.contains("pastebin.com") ->
-              _
-          end
           link = hd(i)
-          IO.puts(link)
+          @endpoints |> Map.keys |> Enum.each(fn key ->
+            if link |> String.contains?(key) do
+              @endpoints[key]["handler"].(link)
+            end
+          end)
+          # IO.puts(link)
           # IO.puts(body)
         end)
       end
     end
   end
   def handle_pastebin(link) do
-    _
+    id = link |> String.split("/", trim: true) |> List.last
+    IO.puts("pastebin")
+    IO.puts(id)
   end
 
   def handle_vocaroo(link) do
-    _
+    id = link |> String.split("/", trim: true) |> List.last
+    IO.puts("vocaroo")
+    IO.puts(id)
   end
 
   def handle_rentry(link) do
-    _
+    id = link |> String.split("/", trim: true) |> List.last
+    IO.puts("rentry")
+    IO.puts(id)
   end
 
   def handle_litter(link) do
-    _
+    id = link |> String.split("/", trim: true) |> List.last
+    IO.puts("litter")
+    IO.puts(id)
   end
 
 end
